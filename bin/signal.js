46e4e7a4ed3f52c8b1a0badd90a1609dbef54348
ok2ju <soulSpringg@gmail.com>
2018-01-04 13:23:52 +0300

    create signal server, add Docker support

---
+++ bin/signal.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const pkg = require('../package.json');
+
+program
+  .version(pkg.version)
+  .option('-p, --port [number]', 'Server port')
+  .parse(process.argv);
+
+io.on('connection', (socket) => {
+  socket.on('join', (room) => {
+    const peers = io.nsps['/'].adaper.rooms[room]
+      ? Object.keys(io.nsps['/'].adaper.rooms[room].sockets)
+      : [];
+
+    socket.emit('peers', peers);
+    socket.join(room);
+  });
+
+  socket.on('signal', (data) => {
+    const client = io.sockets.connected[data.id];
+    client && client.emit('signal', {
+      id: socket.id,
+      signal: data.signal,
+    });
+  });
+});
+
+const port = program.port || 3334;
+http.listen(port, () => {
+  console.log('Listen on port: ', port);
+});
